39363004b6c5ec91499e9f23081af476a9f55d79
Gitfirul Aziz Widiyanto <64318003+gitfirul01@users.noreply.github.com>
2023-02-11 15:12:32 +0700

    Update __main__.ino

--- blood_sensor/receiver/__main__/__main__.ino
+++ blood_sensor/receiver/__main__/__main__.ino
@@ -1,61 +1,58 @@
-/*
+/* DEVICE 2
+  
    |  Arduino  |  GA6-B  |
    |-----------|---------|
    |  D2 (RX)  |  U-TX   |
    |  D3 (TX)  |  U-RX   |
+   
 */
 
 #define gsm_rx 3
 #define gsm_tx 2
-#define buz_pin 5
+#define buz_pin LED_BUILTIN
 
-#include<SoftwareSerial.h>
+#include <SoftwareSerial.h>
 
 bool msg_received = 0;
+String msg;
 
-SoftwareSerial gsm(gsm_tx, gsm_rx);
+SoftwareSerial _gsm(gsm_tx, gsm_rx);
 
 
 void setup() {
   Serial.begin(115200);
-  gsm.begin(115200);
+  _gsm.begin(115200);
 
   pinMode(buz_pin, OUTPUT);
 
-  gsm.println("AT"); //Once the handshake test is successful, it will back to OK
-  updateSerial();
-  gsm.println("AT+CSQ"); //Signal quality test, value range is 0-31 , 31 is the best
-  updateSerial();
-  gsm.println("AT+CCID"); //Read SIM information to confirm whether the SIM is plugged
-  updateSerial();
-  gsm.println("AT+CREG?"); //Check whether it has registered in the network
-  updateSerial();
+  delay(1000);
+
+  _gsm.println("AT");
+  _gsm.println("AT+CMGF=1");
+  _gsm.println("AT+CNMI=1,2,0,0,0");
 }
 
 
 void loop() {
   // device 2 standby sampai menerima pesan dari device 1
-  updateSerial();
-
+  delay(500);
+  while (Serial.available()) {
+    _gsm.write(Serial.read());
+  }
+  while (_gsm.available()) {
+    Serial.write(_gsm.read());
+    msg_received = 1;
+  }
+  
   // ketika menerima pesan, maka buzzer akan berbunyi selama 10 detik
   if (msg_received) {
     for (int i = 0; i < 5; i++) {
       buzzer();
     }
+    msg_received = 0;
   }
 }
 
-
-void updateSerial() {
-  delay(500);
-  while (Serial.available()) {
-    gsm.write(Serial.read()); //Forward what serial port received to GSM
-  }
-  while (gsm.available()) {
-    msg_received = 1;
-    Serial.write(gsm.read()); //Forward what GSM received to serial port
-  }
-}
 
 void buzzer() {
   digitalWrite(buz_pin, HIGH);
