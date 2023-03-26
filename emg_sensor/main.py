60e1a19e38df4949056b066300fb9cef2fc44aa5
Gitfirul Aziz Widiyanto <64318003+gitfirul01@users.noreply.github.com>
2023-03-26 16:23:46 +0700

    Update main.py

--- emg_sensor/main.py
+++ emg_sensor/main.py
@@ -29,34 +29,35 @@ class mainWindow(QMainWindow):
         self.actPotential = 0   # kekuatan litrik rahim saat berkontraksi
         self.maxPotential = 0 
 
-        self.tresh_hold = 120   # batas atas potensi aksi untuk deteksi kontraksi, 120 adalah ujicoba menggunakan otot lengan
+        self.potential_threshold = 120   # batas atas potensi aksi untuk deteksi kontraksi, 120 adalah ujicoba menggunakan otot lengan
 
-        self.countU = 0
+        self.countI = 0
         self.countD = 0
         self.levelD = 0
 
-        self.countInt = 0
-        self.countDur = 0
+        self.countInterval = 0  # variabel untuk menampung nilai lamanya interval
+        self.countDuration = 0  # variabel untuk menampung nilai lamanya durasi
+
+        self.delayms = 150
 
         self.create_lineChart()
 
         # self.serial_port = serial.Serial("/dev/ttyUSB0", 9600)
 
         self.timer = QTimer()
-        self.timer.setInterval(10)  # ms
+        self.timer.setInterval(self.delayms)  # ms
         self.timer.timeout.connect(self.read_data)
         self.timer.start()
 
         self.timerD = QTimer()
-        self.timerD.setInterval(10) # ms
+        self.timerD.setInterval(self.delayms) # ms
         self.timerD.timeout.connect(self.hitung_durasi)
         self.timerD.stop()
 
         self.timerI = QTimer()
-        self.timerI.setInterval(10) # ms
+        self.timerI.setInterval(self.delayms) # ms
         self.timerI.timeout.connect(self.hitung_interval)
         self.timerI.stop()
-        
         
         self.timerF = QTimer()
         self.timerF.setInterval(600000) # ms
@@ -67,47 +68,48 @@ class mainWindow(QMainWindow):
 
 
     def create_lineChart(self):
-        # create chart object
+        ## create chart object
         self.chart = QChart()
         self.chart.setAnimationOptions(QChart.SeriesAnimations)
         # self.chart.setTitle("Electrical Signal")
-        self.chart.legend().setVisible(True)
-        self.chart.legend().setAlignment(Qt.AlignBottom)
-
+        
 
         self.chart_view = QChartView(self.chart)
         self.chart_view.setRenderHint(QPainter.Antialiasing)        
 
-        # # create blank widget to store the graphic
-        # # we do not do this because we have had chart_widget in .ui file
-        # # so we just need to connect that
+        ## create blank widget to store the graphic
+        ## we do not do this because we have had chart_widget in .ui file
+        ## so we just need to connect that
         # chart_widget = QWidget(self)
         # self.setCentralWidget(chart_widget)
 
-        # add the widget to chart_layout
+        ## add the widget to chart_layout
         chart_layout = QVBoxLayout(self.chart_widget)
-        # add chart_view widged to chart_layout
+        ## add chart_view widged to chart_layout
         chart_layout.addWidget(self.chart_view)
 
 
         self.series = QLineSeries()
-        # update series ?
         self.chart.addSeries(self.series)
 
         self.chart.createDefaultAxes()
         self.chart.axisX().setRange(0, 100)
         self.chart.axisY().setRange(0, 200)
+        # self.chart.axisX().setTitleText("time")
+
+        self.chart.legend().setVisible(False)
+        # self.chart.legend().setAlignment(Qt.AlignBottom)
+        # self.chart.legend().markers(self.series)[0].setLabel("Electrical Signal (mV)")
 
 
     def read_data(self): 
-        x = 7 # counter untuk deteksi
-        y = 100 # counter untuk tidak deteksi
-        z = 200 # counter untuk reset deteksi
-
-        # data = self.serial_port.readline().decode().strip() 
-        data = randrange(0, 150)
+        x = 0.07*(1000/self.delayms)  ## counter untuk deteksi        (0,07s)
+        y = 1*(1000/self.delayms)     ## counter untuk tidak deteksi  (1s)
+        z = 2*(1000/self.delayms)     ## counter untuk reset deteksi  (2s)
             
         try: 
+            # data = self.serial_port.readline().decode().strip() 
+            data = randrange(0, 200)
             value = round(float(data), 2)
             self.actPotential = value 
 
@@ -117,55 +119,62 @@ class mainWindow(QMainWindow):
             self.var_maxPotent.setText(f"{self.maxPotential}")
             self.var_actPotent.setText(f"{self.actPotential}")
 
-            if self.actPotential > self.tresh_hold:
-                self.countU += 1
-                self.countD = 0 
+            ## jika terdeteksi sinyal melebihi treshold
+            if self.actPotential > self.potential_threshold:
+                self.countI += 1
+                self.countD = 0
                 self.maxPotential = self.actPotential
-            elif self.actPotential < self.tresh_hold: 
+            ## jika tidak terdeteksi sinyal melebihi treshold
+            elif self.actPotential < self.potential_threshold: 
                 self.countD += 1 
-                if self.actPotential > self.maxPotential : self.maxPotential = self.actPotential
-
-            if self.countU > x and self.levelD == 0:
-                # print("hitung interval dan inisialisasi durasi")
-                if self.actPotential > self.maxPotential : self.maxPotential = self.actPotential
-
-                self.interval = self.countInt/100.0
+                if self.actPotential > self.maxPotential:
+                    self.maxPotential = self.actPotential
+            
+            ## jika terdeteksi kontraksi (jumlah sinyal melebihi angka tertentu dan level deteksi = 0)
+            if self.countI > x and self.levelD == 0:
+                ## update max potential
+                if self.actPotential > self.maxPotential:
+                    self.maxPotential = self.actPotential
+                ## hitung interval
                 self.timerI.stop()
+                self.interval = self.countInterval/(1000/self.delayms)     # convert milisecond of countInterval to second (1 s = 10 * 100 ms)
                 self.frequency += 1
-                # print("Interval: " + str(self.countI))
-                self.countDur = 0
+                ## inisialisasi timer durasi kontraksi
+                self.countDuration = 0
                 self.timerD.start()
-
+                ## ubah level deteksi = 1
                 self.countD = 0
                 self.levelD = 1
-            elif self.countD > y and self.levelD == 1: 
-                if self.actPotential > self.maxPotential : self.maxPotential = self.actPotential
-
-                # print("hitung durasi dan inisialisasi interval")
-                self.duration = self.countDur/100.0
+            ## jika tidak terdeteksi kontraksi lagi dalam 1 detik
+            elif self.countD > y and self.levelD == 1:
+                ## update max potential
+                if self.actPotential > self.maxPotential:
+                    self.maxPotential = self.actPotential
+                ## hitung durasi
                 self.timerD.stop()
-
-                self.kirim_web()
+                self.duration = self.countDuration/(1000/self.delayms)     # convert milisecond of countDuration to second (1 s = 10 * 100 ms)
                 
-                # print("Durasi: " + str(self.countD))
-                self.countInt = 0
+                ## inisialisasi timer interval kontraksi
+                self.countInterval = 0
                 self.timerI.start()
-
-                self.countU = 0
+                ## ubah level deteksi = 0
+                self.countI = 0
                 self.levelD = 0
+                self.kirim_web()
+            ## jika tidak terdeteksi kontraksi lagi dalam 2 detik
             elif self.countD > z:
-                if self.actPotential > self.maxPotential : self.maxPotential = self.actPotential 
-
+                if self.actPotential > self.maxPotential: 
+                    self.maxPotential = self.actPotential 
                 self.countD = 0
-                self.countU = 0 
- 
+                self.countI = 0 
+
             self.series.append(self.counter, value)
             self.counter += 1
  
+
             if len(self.series) > 100:
-                self.series.removePoints(0, 1)
- 
-            self.chart.axisX().setRange(self.counter - 100, self.counter)
+                # self.series.removePoints(0, 1)
+                self.chart.axisX().setRange(self.counter - 100, self.counter)
             self.chart.axisY().setRange(0, 200)
  
             self.chart_view.repaint()
@@ -174,17 +183,17 @@ class mainWindow(QMainWindow):
             pass
     
     def hitung_durasi(self): 
-        self.countDur += 1 
+        self.countDuration += 1 
 
     def hitung_interval(self): 
-        self.countInt += 1
+        self.countInterval += 1
 
     def reset_data(self):
         self.frequency = 0
         self.duration = 0
         self.interval = 0
         self.countD = 0
-        self.countU = 0
+        self.countI = 0
         self.levelD = 0
         self.timerD.stop()
         self.timerI.stop()
@@ -206,12 +215,14 @@ class mainWindow(QMainWindow):
             print('Terjadi kesalahan dalam mengirim data ke server')
 
     def closeEvent(self, event): 
-        self.serial_port.close() 
+        # self.serial_port.close() 
         super().closeEvent(event)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = mainWindow()
     window.show()
+    window.showMaximized()
 
     sys.exit(app.exec_())
