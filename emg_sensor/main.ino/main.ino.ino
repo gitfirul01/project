1439e4a25fe1c9aafdfd88b74720d01da6e872dc
Gitfirul Aziz Widiyanto <64318003+gitfirul01@users.noreply.github.com>
2023-04-03 09:48:01 +0700

    Update main.ino.ino

--- emg_sensor/main.ino/main.ino.ino
+++ emg_sensor/main.ino/main.ino.ino
@@ -1,4 +1,9 @@
-#define vcc 5.0
+#define Vref 3.2    // V
+#define R_G  239.9  // ohm
+#define Gain (1+(49400/R_G))
+
+// float raw = 0, prev_raw = 0;
+// float vol = 0, prev_vol = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -7,7 +12,23 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  float val = analogRead(A0) / 1023.0 * vcc * 1000.0;   // mV
-  Serial.println(val);
-  delay(500);
+  int raw = analogRead(A0);
+  // float vol =  raw / 1023.0 * Vref * 1000.0; // ADC val mV
+  float vol =  (raw / 1023.0 * Vref * 1000.0) / Gain; // actual value mV
+
+  // raw = ema(raw, prev_raw, 0.5);
+  // vol = ema(vol, prev_vol, 0.5);
+
+  // prev_raw = raw;
+  // prev_vol = vol;
+  
+  // Serial.println(raw);
+  Serial.println(vol);
+
+  delay(100);
 }
+
+
+// float ema(float now, float prev, float alpha){
+//   return alpha*now + (1-alpha)*prev;
+// }
