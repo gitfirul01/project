c31513a9263c981d8e2cd8adb9cb7c13b63827df
Gitfirul Aziz Widiyanto <64318003+gitfirul01@users.noreply.github.com>
2023-04-12 18:35:11 +0700

    Update main.ino

--- emg_sensor/main/main.ino
+++ emg_sensor/main/main.ino
@@ -1,34 +1,29 @@
-#define Vref 3.2    // V
-#define R_G  239.9  // ohm
-#define Gain (1+(49400/R_G))
+#define Vref 3.2                  // V
+#define R_G 239.9                 // ohm
+#define Gain (1 + (49400 / R_G))  // https://www.analog.com/media/en/technical-documentation/data-sheets/ad8221.pdf
 
-// float raw = 0, prev_raw = 0;
-// float vol = 0, prev_vol = 0;
+float raw = 0;//, prev_raw = 0;
+float vol = 0;//, prev_vol = 0;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int raw = analogRead(A0);
-  // float vol =  raw / 1023.0 * Vref * 1000.0; // ADC val mV
-  float vol =  (raw / 1023.0 * Vref * 1000.0) / Gain; // actual value mV
+  raw = analogRead(A0);
+  // vol = raw / 1023.0 * Vref * 1000.0;           // ADC val mV
+  vol = (raw / 1023.0 * Vref * 1000.0) / Gain;  // actual val mV
 
-  // raw = ema(raw, prev_raw, 0.5);
-  // vol = ema(vol, prev_vol, 0.5);
+  // ema(&raw, &prev_raw, 0.5);
+  // ema(&vol, &prev_vol, 0.5);
 
-  // prev_raw = raw;
-  // prev_vol = vol;
-  
   // Serial.println(raw);
   Serial.println(vol);
 
   delay(100);
 }
 
-
-// float ema(float now, float prev, float alpha){
-//   return alpha*now + (1-alpha)*prev;
+// void ema(float* now, float* prev, float alpha) {
+//   now = alpha * now + (1 - alpha) * prev;
+//   prev = now;
 // }
